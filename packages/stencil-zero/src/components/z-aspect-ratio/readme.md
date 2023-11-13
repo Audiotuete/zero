0df5ce0966fb030304b8207fa3a92cbdbbaff11f
Jan <jan_rock@hotmail.de>
2023-11-13 15:52:22 +0100

    create: z-aspect-ratio component

---
+++ packages/stencil-zero/src/components/z-aspect-ratio/readme.md
@@ -0,0 +1,18 @@
+# z-aspect-ratio
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property   | Attribute   | Description | Type     | Default     |
+| ---------- | ----------- | ----------- | -------- | ----------- |
+| `maxWidth` | `max-width` |             | `string` | `undefined` |
+| `ratio`    | `ratio`     |             | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ packages/stencil-zero/src/components/z-aspect-ratio/z-aspect-ratio.tsx
@@ -0,0 +1,26 @@
+import { Component, Prop, Host, h } from '@stencil/core'
+
+@Component({
+  tag: 'z-aspect-ratio',
+  styleUrl: 'z-aspect-ratio.css',
+  shadow: false,
+})
+export class AspectRatio {
+  @Prop() ratio: string
+  @Prop() maxWidth: string
+
+  calculatePaddingTop = () => {
+    const [w, h] = this.ratio.split(':').map(v => parseInt(v))
+    return { paddingTop: (h / w) * 100 + '%' }
+  }
+
+  render() {
+    return (
+      <Host style={{ maxWidth: this.maxWidth, display: 'block' }}>
+        <div class="zero-aspect-ratio-container-inner" style={this.calculatePaddingTop()}>
+          <slot />
+        </div>
+      </Host>
+    )
+  }
+}
