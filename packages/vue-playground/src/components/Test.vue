d29c5d07db6c1ebf73b2ba5e8beffcc524085d7e
Jan <jan_rock@hotmail.de>
2023-11-17 14:08:32 +0100

    create: dynamic Vue Test comp

---
+++ packages/vue-playground/src/components/Test.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, render } from "vue"
+
+const helloWorld = () => {
+  return "Hello World"
+}
+
+const renderStaticComponent = (component: any) => {
+  const el = document.createElement("div")
+  render(component.render(), el)
+
+  return el.firstChild
+}
+
+const variable = ref(10)
+</script>
+
+<template>
+  <div @click="variable++">{{ helloWorld() }} {{ variable }}</div>
+</template>
+
+<style scoped></style>
